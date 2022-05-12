a6f699c007d8b84399add5465bed7b20427c6a46
DovydasMM <dovistk@gmail.com>
2022-05-12 11:21:30 +0300

    Save

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-function boxCreate(amount){
+function boxCreate(amount){  // function to create box grid
 let div="";
-
 for (let i=0;i<(amount**2);i++){
 div=document.createElement('div');
 div.classList.add('boxes');
 container.appendChild(div)
 }
-
+//hoverButton()
 }
-
-
+// code for box color change, when hovered
+function hoverButton() {
 const container = document.querySelector('#container');
 container.addEventListener('mouseover', function (e) {
 if (e.target.classList.contains('boxes')) {
-    console.log("yra");
-    e.target.classList.add('boxes-hover')
+ e.target.classList.add('hover')
 }
+});
+};
+
+
+//restart button
+const btnRestart = document.querySelector('.reload');
+btnRestart.addEventListener('click',()=> {
+    location.reload()
 });
 
 
-
-
-boxCreate(16);
+boxCreate(10);
 
